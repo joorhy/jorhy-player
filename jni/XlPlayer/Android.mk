fde76df4d9625d83bba721b727589d04e9710f2c
joorhy <joorhy@gmail.com>
2016-11-11 21:44:06 +0800

    new file:   jni/XlPlayer/Android.mk

---
+++ jni/XlPlayer/Android.mk
@@ -0,0 +1,31 @@
+LOCAL_PATH := $(call my-dir)
+
+###########################
+#
+# XlPlayer shared library
+#
+###########################
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := XlPlayer
+LOCAL_ARM_MODE=arm
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/include \
+					$(LOCAL_PATH)/include/libpng \
+					$(LOCAL_PATH)/Android/SDL2/include \
+					$(LOCAL_PATH)/Android/OpenH264/include
+
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_C_INCLUDES)
+
+LOCAL_SRC_FILES := \
+	$(subst $(LOCAL_PATH)/,, \
+	$(wildcard $(LOCAL_PATH)/src/*.c) \
+	$(wildcard $(LOCAL_PATH)/src/libpng/*.c))
+
+LOCAL_CFLAGS += -DGL_GLEXT_PROTOTYPES
+LOCAL_LDFLAGS  := -L $(LOCAL_PATH)/Android/SDL2/lib -L $(LOCAL_PATH)/Android/OpenH264/lib
+LOCAL_LDLIBS := -ldl -llog -lz -lSDL2 -lopenh264
+
+include $(BUILD_SHARED_LIBRARY)
+
+LOCAL_SHARED_LIBRARIES = XlPlayer 
